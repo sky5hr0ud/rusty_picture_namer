480ec1100097a6c076d334272172eb3e14a7ace9
sky5hr0ud <46011256+sky5hr0ud@users.noreply.github.com>
2022-03-07 16:52:47 -0800

    filenames are working.

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,31 @@ use std::cmp::Reverse;
 
 fn main() {
     println!("Hello, world!");
-    let folder_path = "/home/amel/Pictures/Pictures to rename/";
+    let folder_path = "C:\\Users\\distu\\Downloads\\New folder\\Random 2022\\";
     println!("{:?}", file_namer(folder_path));
 }
 
 fn file_namer(folder_path: &str) -> Result<bool, Error> {
     std::env::set_current_dir(folder_path)?;
     let sys_time = SystemTime::now();
-    //let mut paths: Vec<_> = fs::read_dir(folder_path).unwrap().map(|r| r.unwrap()).collect();
     let mut paths: Vec<_> = fs::read_dir(folder_path).unwrap().filter_map(Result::ok).collect();
     println!("{:?}", paths);
     paths.retain(|path| fs::metadata(path.path()).unwrap().is_file());
-    //paths.sort_by_key(|file| (sys_time - fs::metadata(file.path()).unwrap().modified().unwrap()));
+    let file_names = ["jpg"];//, "gif", "png"];
+    //let mut pictures: Vec<_>; 
+    for file_name in file_names {
+        paths.retain(|path| path.path().extension().unwrap() == file_name);
+    }
     paths.sort_by_key(|path| Reverse(sys_time.duration_since(fs::metadata(path.path()).unwrap().modified().unwrap()).unwrap().as_millis()));
-    //paths.sort_by_key(|file| fs::metadata(file.path()).unwrap().modified().unwrap().as_millis());
     println!("{:?}", paths);
     for (index, path) in paths.iter().enumerate() {
         let file = path.path();
         //println!("{:?}", path);
         //println!("{:?}", file);
         //println!("{:?}", fs::metadata(&file)?.file_type());
-        println!("{:?}", file);
-        println!("{:?}", path);
-        //println!("{:?}", file.file_name());
-        //println!("{:?}", file.extension());
-        fs::rename(file, index.to_string())?;
+        println!("{:?}", file.file_name());
+        println!("{:?}", file.extension());
+        //fs::rename(file, index.to_string())?;
     }
 
 
