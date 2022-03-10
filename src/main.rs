3a44a036cd630f4608330437b7c99fefbe56a0f6
sky5hr0ud <46011256+sky5hr0ud@users.noreply.github.com>
2022-03-09 17:34:27 -0800

    finished file walker
    can now walk through directories.
    need: 1. args input
    2. error handling
    3. clean code
    4. document

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,35 @@ use std::io::Error;
 use std::fs;
 use std::time::SystemTime;
 use std::cmp::Reverse;
+use walkdir::WalkDir;
 
 fn main() {
     println!("Hello, world!");
-    let folder_path = "C:\\Users\\distu\\Downloads\\New folder\\Random 2021\\Housewarming Party";
+    let folder_path = "C:\\Users\\distu\\Downloads\\New folder\\Random 2021\\";
     let filetypes_path = "C:\\Users\\distu\\Documents\\Scripts\\Git\\picture_namer\\_list_of_filetypes.txt";
-    println!("{:?}", file_namer(folder_path, filetypes_path));
-    //println!("{:?}", get_filetypes(filetypes_path));
+    let result = match directory_walker(folder_path, filetypes_path) {
+        Ok(result) => result,
+        Err(_) => false
+    };
 }
 
-fn file_namer(folder_path: &str, filetypes_path: &str) -> Result<bool, Error> {
+fn directory_walker(folder_path: &str, filetypes_path: &str) -> Result<bool, Error> {
+    let mut directories: Vec<walkdir::DirEntry> = WalkDir::new(folder_path).into_iter().filter_map(|e| e.ok()).collect();
+    directories.retain(|entry| fs::metadata(entry.path()).unwrap().is_dir());
+    for directory in directories {
+        file_namer(directory.path(), filetypes_path)?;
+    }
+    return Ok(true)
+}
+
+fn file_namer(folder_path: &std::path::Path, filetypes_path: &str) -> Result<bool, Error> {
     std::env::set_current_dir(folder_path)?;
     let sys_time = SystemTime::now();
     let mut paths: Vec<fs::DirEntry> = fs::read_dir(folder_path).unwrap().filter_map(Result::ok).collect();
     paths.retain(|path| fs::metadata(path.path()).unwrap().is_file());
-    let file_types = get_filetypes(filetypes_path)?;//["jpg", "png", "gif"];
+    let file_types = get_filetypes(filetypes_path)?;
     paths.retain(|path| vec_contains(&file_types, path.path().extension().unwrap().to_str().unwrap()));
     paths.sort_by_key(|path| Reverse(sys_time.duration_since(fs::metadata(path.path()).unwrap().modified().unwrap()).unwrap().as_millis()));
-    println!("{:?}", paths);
     let mut file_count = file_counter(&paths)?; // try out the naming operation to see how many files it renames
     let lead_zeros = lead_zeros(5, file_count.1); // want to make sure that we have enough padding
     let mut files_renamed: u32 = 0;
@@ -39,7 +50,7 @@ fn file_namer(folder_path: &str, filetypes_path: &str) -> Result<bool, Error> {
             files_renamed += 1;
         }
     }
-    println!("Renamed {} files in {}", files_renamed, folder_path);
+    println!("Renamed {} files in {}", files_renamed, folder_path.display());
     return Ok(true)
 }
 
