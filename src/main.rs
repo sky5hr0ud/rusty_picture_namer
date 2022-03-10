d2f96ec8962de95d6eebb957063af393157a530c
sky5hr0ud <46011256+sky5hr0ud@users.noreply.github.com>
2022-03-10 11:28:22 -0800

    implemented args

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
-use std::io::Error;
+use std::error::Error;
 use std::fs;
 use std::time::SystemTime;
 use std::cmp::Reverse;
+use std::env;
 use walkdir::WalkDir;
 
 fn main() {
-    println!("Hello, world!");
-    let folder_path = "C:\\Users\\distu\\Downloads\\New folder\\Random 2021\\";
-    let filetypes_path = "C:\\Users\\distu\\Documents\\Scripts\\Git\\picture_namer\\_list_of_filetypes.txt";
-    let result = match directory_walker(folder_path, filetypes_path) {
-        Ok(result) => result,
-        Err(_) => false
-    };
+    if env::args().len() < 2 {
+        println!("Need two args! <folder_path> <list_of_filetypes>");
+        std::process::exit(1);
+    }
+    let folder_path = env::args().nth(1).unwrap();
+    let filetypes_path = env::args().nth(2).unwrap();
+    let result = directory_walker(&folder_path, &filetypes_path);
+    println!("{:?}", result);
+    std::process::exit(0);
 }
 
-fn directory_walker(folder_path: &str, filetypes_path: &str) -> Result<bool, Error> {
+fn directory_walker(folder_path: &str, filetypes_path: &str) -> Result<bool, Box<dyn Error>> {
+    println!("Preparing to rename files in {}", folder_path);
+    let filetypes = get_filetypes(filetypes_path)?;
     let mut directories: Vec<walkdir::DirEntry> = WalkDir::new(folder_path).into_iter().filter_map(|e| e.ok()).collect();
     directories.retain(|entry| fs::metadata(entry.path()).unwrap().is_dir());
     for directory in directories {
-        file_namer(directory.path(), filetypes_path)?;
+        file_namer(directory.path(), &filetypes)?;
     }
     return Ok(true)
 }
 
-fn file_namer(folder_path: &std::path::Path, filetypes_path: &str) -> Result<bool, Error> {
+fn file_namer(folder_path: &std::path::Path, filetypes: &Vec<String>) -> Result<bool, Box<dyn Error>> {
     std::env::set_current_dir(folder_path)?;
     let sys_time = SystemTime::now();
     let mut paths: Vec<fs::DirEntry> = fs::read_dir(folder_path).unwrap().filter_map(Result::ok).collect();
     paths.retain(|path| fs::metadata(path.path()).unwrap().is_file());
-    let file_types = get_filetypes(filetypes_path)?;
-    paths.retain(|path| vec_contains(&file_types, path.path().extension().unwrap().to_str().unwrap()));
+    paths.retain(|path| vec_contains(&filetypes, path.path().extension().unwrap().to_str().unwrap()));
     paths.sort_by_key(|path| Reverse(sys_time.duration_since(fs::metadata(path.path()).unwrap().modified().unwrap()).unwrap().as_millis()));
     let mut file_count = file_counter(&paths)?; // try out the naming operation to see how many files it renames
     let lead_zeros = lead_zeros(5, file_count.1); // want to make sure that we have enough padding
@@ -55,7 +59,7 @@ fn file_namer(folder_path: &std::path::Path, filetypes_path: &str) -> Result<boo
 }
 
 /// Count the files to be renamed. Some files may already have the directory name already prepended so no rename needs to be done.
-fn file_counter(paths: &Vec<fs::DirEntry>) -> Result<(u32, u32),  Error> {
+fn file_counter(paths: &Vec<fs::DirEntry>) -> Result<(u32, u32),  Box<dyn Error>> {
     let mut files: u32 = 0;
     let mut files_already_modified: u32 = 0;
     for path in paths {
@@ -72,7 +76,7 @@ fn file_counter(paths: &Vec<fs::DirEntry>) -> Result<(u32, u32),  Error> {
     return Ok((files_already_modified, files))
 }
 
-fn get_filetypes(filetypes_file: &str) -> Result<Vec<String>, Error> {
+fn get_filetypes(filetypes_file: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let mut contents = fs::read_to_string(filetypes_file)?;
     contents = contents.to_ascii_lowercase() + &contents.to_ascii_uppercase();
     let mut contents_vec: Vec<String> = contents.split_whitespace().map(str::to_string).collect();
